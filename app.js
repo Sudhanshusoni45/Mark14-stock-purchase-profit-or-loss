767ce68917a31e7baabbfafad679e7505735ac85
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2021-09-30 16:54:32 +0530

    initial_commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var inputBuyingPriceRef = document.querySelector("#input-buying-price");
+var inputNoOfStockRef = document.querySelector("#input-number-shares");
+var inputCurrentPriceRef = document.querySelector("#input-current-price");
+var btnRef = document.querySelector("#btn-calculate");
+btnRef.addEventListener("click", clickHandler);
+
+var divOutputRef = document.querySelector("#div-output");
+
+function clickHandler() {
+  var buyingPrice = inputBuyingPriceRef.value;
+  var noOfStocks = inputNoOfStockRef.value;
+  var currentPrice = inputCurrentPriceRef.value;
+
+  var profitPerShare = currentPrice - buyingPrice;
+  console.log("profitPerShare:", profitPerShare);
+
+  calculate(profitPerShare, noOfStocks);
+}
+
+function calculate(profitPerShare, noOfStocks) {
+  var netPrice = profitPerShare * noOfStocks;
+
+  if (netPrice === 0) {
+    divOutputRef.innerText = netPrice + " No pain no gain, No gain no pain";
+  }
+  if (netPrice > 0) {
+    divOutputRef.innerText = netPrice + " Kai Paisa";
+  }
+  if (netPrice < 0) {
+    divOutputRef.innerText = netPrice + " Hold Kar Bhai";
+  }
+}
